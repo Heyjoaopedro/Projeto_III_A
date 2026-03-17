4110e53add7104e710c62639309a5286fbe461b9
Rapadura-gb <110193299+need0auth@users.noreply.github.com>
2026-03-17 06:28:40 +0000

    Estrutura Base do projeto usando Maven

---
+++ projeto3a/src/main/java/br/com/grupo3/Main.java
@@ -0,0 +1,13 @@
+package br.com.grupo3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
