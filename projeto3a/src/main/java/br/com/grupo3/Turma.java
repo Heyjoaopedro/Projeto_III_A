00b08b011a8dfd3a4359f51bfd93072d9aa4e617
Rapadura-gb <110193299+need0auth@users.noreply.github.com>
2026-03-17 06:39:32 +0000

    Criação da classe Turma com construtor e getters

---
+++ projeto3a/src/main/java/br/com/grupo3/Turma.java
@@ -0,0 +1,27 @@
+package br.com.grupo3.model;
+
+public class Turma {
+    private String codigo;
+    private String etapaEnsino; // infantil, fundamental, médio
+    private int ano;
+    private int limiteVagas;
+    private int numeroMatriculados;
+
+    public Turma(String codigo, String etapaEnsino, int ano, int limiteVagas) {
+        this.codigo = codigo;
+        this.etapaEnsino = etapaEnsino;
+        this.ano = ano;
+        this.limiteVagas = limiteVagas;
+        this.numeroMatriculados = 0; // Começa vazia
+    }
+
+    // Getters básicos
+    public String getCodigo() { return codigo; }
+    public String getEtapa() { return etapaEnsino; }
+
+    @Override
+    public String toString() {
+        return String.format("Cód: %s | Etapa: %s | Ano: %d | Vagas: %d/%d", 
+            codigo, etapaEnsino, ano, numeroMatriculados, limiteVagas);
+    }
+}
